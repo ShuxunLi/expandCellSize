da6aa510ab9d3e0a1d17b92e445347216226f721
VirgilXun <lishuxun1992@foxmail.com>
2016-05-18 17:30:48 +0800

    1. add project

---
+++ expandedCellSize/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  expandedCellSize
+//
+//  Created by 聚财道 on 16/5/18.
+//  Copyright © 2016年 jucaidao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ expandedCellSizeTests/expandedCellSizeTests.m
@@ -0,0 +1,39 @@
+//
+//  expandedCellSizeTests.m
+//  expandedCellSizeTests
+//
+//  Created by 聚财道 on 16/5/18.
+//  Copyright © 2016年 jucaidao. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface expandedCellSizeTests : XCTestCase
+
+@end
+
+@implementation expandedCellSizeTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
